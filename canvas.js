ae820e6579fd09f37e22214f88d7e6b0696e5a4f
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2020-08-16 18:22:48 -0500

    Test all operations successfully

--- canvas.js
+++ canvas.js
@@ -3,4 +3,5 @@ const $canvas = document.querySelector("canvas")
 const ctx = $canvas.getContext("2d")
 const $startButton = document.querySelector(".start-button")
 
-
+// Math variables initialization
+let x0 = 0, operand = 0, operandString = '', result = 0
--- helpers.js
+++ helpers.js
@@ -2,23 +2,22 @@
 
 // Get random numbers according to the specified level (level 1 for now)
 function getRandomNumbers(level){
-    let x0 = 0
-    let operand = Math.floor(Math.random()*(3)+1)
-    let y = 0
-    let operandString = ''
-    let result = 0
     switch(level){
         case 1:
-            // Result can't be higher than 100
+            // Result can't be higher than 100, addition
+            operand = Math.floor(Math.random()*1+1)
             return toggleByOperand(operand, 100)
         case 2:
-            // Result can't be higher than 300
+            // Result can't be higher than 300, addition
+            operand = Math.floor(Math.random()*1+1)
             return toggleByOperand(operand, 300)
         case 3:
-            // Result can't be higher than 500
+            // Result can't be higher than 500, addition and multiplication
+            operand = Math.floor(Math.random()*3+1)
             return toggleByOperand(operand, 500)
         case 4: 
-            // Result can't be higher than 1000
+            // Result can't be higher than 1000, addition and multiplication
+            operand = Math.floor(Math.random()*3+1)
             return toggleByOperand(operand, 1000)
         default:
             return 'Please select a correct level'
@@ -31,35 +30,38 @@ function toggleByOperand(operand, level){
             operandString = '+'
             x0 = Math.floor(Math.random()*(level-1)+1)
             y = Math.floor(Math.random()*(level-1)+1)
-            while(y+x0 >= 100){
+            while(y+x0 >= level){
                 y = Math.floor(Math.random()*(level-1)+1)
             }
-            result = y + x0
-            return [x0, y, result]
+            result = x0 + y
+            return [x0, y, operandString, result]
             break
         case 2:
             operandString = '-'
             x0 = Math.floor(Math.random()*(level-1)+1)
             y = Math.floor(Math.random()*x0 + 1) 
-            result = y - x0
-            return [x0, y, result]
+            result = x0 - y
+            return [x0, y, operandString, result]
             break
         case 3:
             operandString = '*'
-            x0 = Math.floor(Math.random()*(level-1)+1)
+            x0 = Math.floor(Math.random()*(level/2-1)+1)
             y = Math.floor(level/x0)
-            result = y * x0
-            return [x0, y, result]
+            while(x0*y >= level){
+                y = Math.floor(Math.random()*(level-2)+2)
+            }
+            result = x0 * y
+            return [x0, y, operandString, result]
             break
         case 4: 
             operandString = '/'
             x0 = Math.floor(Math.random()*(level-1)+1)
-            y = Math.floor(Math.random()*level-1+1)
+            y = Math.floor(Math.random()*(level-1)+1)
             while(y%level != 0){
                 y = Math.floor(Math.random()*level-1+1)
             }
-            result = y * x0
-            return [x0, y, result]
+            result = x0 / y
+            return [x0, y, operandString, result]
             break
     }
 }
